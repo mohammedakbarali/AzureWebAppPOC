cafa9b8ff707d7e20a731579d27f2036bb818764
Ali <MohammedAkbar.Ali_HCL@kantar.com>
2018-12-05 18:26:52 +0530

    azure testing poc

--- azureWebApp/Controllers/HomeController.cs
+++ azureWebApp/Controllers/HomeController.cs
@@ -4,27 +4,106 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Newtonsoft.Json;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using azureWebApp.Models;
+
 namespace azureWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private string ClientId = System.Web.Configuration.WebConfigurationManager.AppSettings["Google.ClientID"];
+        private string SecretKey = System.Web.Configuration.WebConfigurationManager.AppSettings["Google.SecretKey"];
+        private string RedirectUrl = System.Web.Configuration.WebConfigurationManager.AppSettings["Google.RedirectUrl"];
+          
+
+        /// <summary>    
+        /// Returns login page if user is not logged in else return user profile    
+        /// </summary>    
+        /// <returns>return page</returns>  
+        public async Task<ActionResult> Index()
         {
-            return View();
+            string token = (string)Session["user"];
+            if (string.IsNullOrEmpty(token))
+            {
+                return View();
+            }
+            else
+            {
+                return View("UserProfile", await GetuserProfile(token));
+            }
         }
 
-        public ActionResult About()
+        /// <summary>  
+        /// Hit Google API to get access code  
+        /// </summary>  
+        public void LoginUsingGoogle()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            Response.Redirect($"https://accounts.google.com/o/oauth2/v2/auth?client_id={ClientId}&response_type=code&scope=openid%20email%20profile&redirect_uri={RedirectUrl}&state=abcdef&prompt=consent");
         }
 
-        public ActionResult Contact()
+        [HttpGet]
+        public ActionResult SignOut()
         {
-            ViewBag.Message = "Your contact page.";
+            Session["user"] = null;
+            return View("Index");
+        }
 
-            return View();
+        /// <summary>  
+        /// Listen response from Google API after user authorization  
+        /// </summary>  
+        /// <param name="code">access code returned from Google API</param>  
+        /// <param name="state">A value passed by application to prevent Cross-site request forgery attack</param>  
+        /// <param name="session_state">session state</param>  
+        /// <returns></returns>  
+        [HttpGet]
+        public async Task<ActionResult> SaveGoogleUser(string code, string state, string session_state)
+        {
+            if (string.IsNullOrEmpty(code))
+            {
+                return View("Error");
+            }
+
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.googleapis.com")
+            };
+            var requestUrl = $"oauth2/v4/token?code={code}&client_id={ClientId}&client_secret={SecretKey}&redirect_uri={RedirectUrl}&grant_type=authorization_code";
+
+            var dict = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/x-www-form-urlencoded" }
+            };
+            var req = new HttpRequestMessage(HttpMethod.Post, requestUrl) { Content = new FormUrlEncodedContent(dict) };
+            var response = await httpClient.SendAsync(req);
+            var token = JsonConvert.DeserializeObject<GmailToken>(await response.Content.ReadAsStringAsync());
+            Session["user"] = token.AccessToken;
+            var obj = await GetuserProfile(token.AccessToken);
+
+            //IdToken property stores user data in Base64Encoded form  
+            //var data = Convert.FromBase64String(token.IdToken.Split('.')[1]);  
+            //var base64Decoded = System.Text.ASCIIEncoding.ASCII.GetString(data);  
+
+            return View("UserProfile", obj);
+        }
+
+        /// <summary>  
+        /// To fetch User Profile by access token  
+        /// </summary>  
+        /// <param name="accesstoken">access token</param>  
+        /// <returns>User Profile page</returns>  
+        public async Task<UserProfile> GetuserProfile(string accesstoken)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://www.googleapis.com")
+            };
+            string url = $"https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token={accesstoken}";
+            var response = await httpClient.GetAsync(url);
+            return JsonConvert.DeserializeObject<UserProfile>(await response.Content.ReadAsStringAsync());
         }
     }
 }
