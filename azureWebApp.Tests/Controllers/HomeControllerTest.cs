fcd771c216f0f5311b27bd39f0cad12140dbef2b
mohammedakbarali <45233132+mohammedakbarali@users.noreply.github.com>
2019-04-25 17:40:38 +0530

    Update HomeControllerTest.cs

--- azureWebApp.Tests/Controllers/HomeControllerTest.cs
+++ azureWebApp.Tests/Controllers/HomeControllerTest.cs
@@ -35,7 +35,7 @@ namespace azureWebApp.Tests.Controllers
             ViewResult result = controller.About() as ViewResult;
 
             // Assert
-            Assert.AreEqual("Your application description page.", result.ViewBag.Message);
+            Assert.AreEqual("Your application description page1.", result.ViewBag.Message);
            
         }
 
